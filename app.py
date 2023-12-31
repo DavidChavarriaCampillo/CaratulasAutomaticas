ab4ba8a994d7bc9f7c0ae42ccfe94508368eeaf4
SrTonyChop <campillo-10@hotmail.com.ar>
2023-12-31 16:31:08 -0500

    spript update and bug fix when putting the covers

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import shutil
 import os
 
-RTDISCO = ":/Aurora 0.5b - Release Package/Data/GameData"
-RTCARATULAS = "C:/Users/SrTonyChop/Desktop/solido/Discos duros/Caratulas"
+# RTDISCO = ":/Aurora 0.5b - Release Package/Data/GameData"
+RTDISCO = ":/Aurora/Data/GameData"
+RTCARATULAS = "C:/Users/SrTonyChop/Desktop/GitHub/CaratulasAutomaticas/car"
 
 Disco = input("Ingrese la letra donde esta montado el disco:")
 Disco = Disco.upper()
@@ -11,12 +12,16 @@ ListCaraulas = os.listdir(RTCARATULAS)
 ListDisco = os.listdir(Disco + RTDISCO)
 
 for caratula in ListCaraulas:
-    caratula = caratula.split(".")[0].split("GC")
+    caratula = caratula.split(".")[0].split("GC")[1]
     for datos in ListDisco:
         datos = datos.split("_")
-        if caratula[1] == datos[0]:
+        if caratula == datos[0]:
             try:
-                os.remove(Disco + RTDISCO + "/" +  datos[0] + "_" + datos[1] + "/GC" + datos[0] + ".asset")
-                shutil.copy(RTCARATULAS + "/" + "GC" + caratula[1] + ".asset", Disco + RTDISCO + "/" +  datos[0] + "_" + datos[1] + "/GC" + datos[0] + ".asset")
+                exist = os.path.exists(Disco + RTDISCO + "/" +  datos[0] + "_" + datos[1] + "/GC" + caratula + ".asset")
+                if exist:
+                    os.remove(Disco + RTDISCO + "/" +  datos[0] + "_" + datos[1] + "/GC" + caratula + ".asset")
+                shutil.copy(RTCARATULAS + "/" + "GC" + caratula + ".asset", Disco + RTDISCO + "/" +  datos[0] + "_" + datos[1] )
             except:
-                print("No hay prblema es el cod 2 de la usb o no encontro el GC de en alguna carpeta")
+                print("Fallo al remover o copiar el rchivo GC en el disco duro con juegos")
+
+print('Caratulas instaladas con exito')
